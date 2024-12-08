7262b550f40f1e3161bffbb6db06ed8edeae9c0f
Devanshu Kushwah <devanshukushwah.001@gmail.com>
2024-12-08 17:18:47 +0530

    fixing lombok issue and add log4j2 for logging.

--- src/main/java/com/formsapp/controller/FormController.java
+++ src/main/java/com/formsapp/controller/FormController.java
@@ -3,18 +3,21 @@ package com.formsapp.controller;
 import com.formsapp.common.AppMessage;
 import com.formsapp.model.Form;
 import com.formsapp.model.core.CustomResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/forms")
 public class FormController extends BaseController {
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<CustomResponse<Form>> getForms() {
+        log.info("successfully get form");
         return responseOk(new Form());
     }
 

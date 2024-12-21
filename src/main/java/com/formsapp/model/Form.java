adacba6355c40f416457d23bd84165e5be94b2d6
Devanshu Kushwah <devanshukushwah.001@gmail.com>
2024-12-21 16:35:11 +0530

    add onchange and oncreate date and time for form.

--- src/main/java/com/formsapp/model/Form.java
+++ src/main/java/com/formsapp/model/Form.java
@@ -10,16 +10,61 @@ import javax.persistence.Id;
 import javax.persistence.Transient;
 import java.util.List;
 
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Entity representing a form with fields for title, description,
+ * create time, and update time, along with transient form fields.
+ */
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Form {
+
     @Id // Primary key
     private String formId;
+
     private String title;
+
     private String description;
+
+    /**
+     * Timestamp when the form was created.
+     * Automatically populated when the entity is persisted.
+     */
+    @Column(updatable = false)
+    private Date createTime;
+
+    /**
+     * Timestamp when the form was last updated.
+     * Automatically updated whenever the entity is updated.
+     */
+    private Date changedTime;
+
+    /**
+     * List of form fields associated with the form.
+     * This field is not persisted in the database.
+     */
     @Transient
     private List<FormField> formFields;
+
+    /**
+     * Callback to set createTime and changedTime before persisting.
+     */
+    @PrePersist
+    protected void onCreate() {
+        this.createTime = new Date();
+    }
+
+    /**
+     * Callback to update changedTime before updating the entity.
+     */
+    @PreUpdate
+    protected void onUpdate() {
+        this.changedTime = new Date();
+    }
 }
+
